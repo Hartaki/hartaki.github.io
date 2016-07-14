5f923d413c7ce63cf94940285eabbe7a2aed2dca
jochen <vomc8one@gmail.com>
2016-07-14 03:33:10 -0400

    adding that weird phaser site

---
+++ dev/hartaki.js
@@ -0,0 +1,10 @@
+ $('document').ready(function() {
+             $(document).scroll(function(){
+             if (document.documentElement.clientHeight + 
+             $(document).scrollTop() >= document.body.offsetHeight ) {
+
+              $(document).scrollTop(0);
+            
+          }
+             });
+          }); 
